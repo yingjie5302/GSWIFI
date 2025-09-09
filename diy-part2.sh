80c7511449feab34d770a7d6f8758c808ad2046a
yingjie5302 <57897432+yingjie5302@users.noreply.github.com>
2025-09-09 10:45:25 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -14,3 +14,6 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 git clone https://github.com/EOYOHOO/UA2F.git package/UA2F
 git clone https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+
+./scripts/feeds update -a
+./scripts/feeds install -a
